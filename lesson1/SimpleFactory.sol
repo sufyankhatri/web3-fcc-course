f66ae918d9b4039ee208c58cc7ac356649a3bd01
sufyankhatri <sufyankhatree@gmail.com>
2022-08-12 07:41:10 +0500

    lesson 1 programms added

---
+++ lesson1/SimpleFactory.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
+        //ABI
+        simpleStorageArray[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        //ABI
+        return simpleStorageArray[_simpleStorageIndex].retrieve();
+    }
+
+
+
+}
+
